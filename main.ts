e4c45630f21dbdef6b2556d1d8fb6d923a75035e
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2025-05-14 07:15:42 -0700

    w

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
-import { hCaptchaLoader } from '@hcaptcha/loader';
+import hCaptchaLoader  from '@hcaptcha/loader';
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://piukosdnirsgphzdyjan.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpdWtvc2RuaXJzZ3BoemR5amFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwODc3MDIsImV4cCI6MjA2MjY2MzcwMn0.eIvvAayqroZzDpmlz21uRpllEAJhV_vrQtBJccxcvLw')
+
 
 async function signInWithEmail(email) {
     const { data, error } = await supabase.auth.signInWithOtp({
