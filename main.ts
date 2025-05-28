955aab17ba857e376fb237e6f264f1a8e7be72cb
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2025-05-27 20:52:53 -0700

    changed stuff

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import { hCaptchaLoader } from "@hcaptcha/loader";
 import { customAlphabet } from "nanoid";
 import { checkLoginUser, checkLogin, onAuthChange } from "./login";
-import { cyrb53, humanize, onAllFinished, once, requireFinishedAsync, showToast } from "./utils";
+import {
+    cyrb53,
+    humanize,
+    onAllFinished,
+    once,
+    requireFinishedAsync,
+    showToast,
+} from "./utils";
 import { supabase, Types as dbTypes } from "./supabase-client";
 
 export const nanoid = customAlphabet("1234567890abcdef", 30);
@@ -27,7 +34,9 @@ const channel = supabase.realtime.channel("messages", {
 
 const chat = document.getElementById("chat") as HTMLDivElement;
 const chatForm = document.getElementById("chat-form") as HTMLFormElement;
-const notifSound = document.getElementById("notification-sound") as HTMLAudioElement;
+const notifSound = document.getElementById(
+    "notification-sound"
+) as HTMLAudioElement;
 
 once(async () => {
     chatForm.addEventListener("submit", async (event) => {
@@ -49,7 +58,8 @@ once(async () => {
         if (message.length > 500) {
             showToast({
                 title: "Error",
-                message: "Message is too long. Maximum length is 500 characters.",
+                message:
+                    "Message is too long. Maximum length is 500 characters.",
                 type: "error",
                 duration: 5000,
             });
@@ -91,109 +101,151 @@ once(async () => {
         }
     });
 
-    channel.on(
-        "postgres_changes",
-        { event: "*", schema: "public", table: "messages" },
-        async (payload) => {
-            console.log("New message received:", payload);
-            if ((payload.new as dbTypes.Database["public"]["Tables"]["messages"]["Row"]).user_id === (await checkLoginUser())?.id) {
-                return;
-            }
-            switch (payload.eventType) {
-                case "INSERT": 
-                    notifSound.play().catch((error) => {
-                        console.error("Error playing notification sound:", error);
-                    });
-                    try {
-                        const message = row2Msg(
+    channel
+        .on(
+            "postgres_changes",
+            { event: "*", schema: "public", table: "messages" },
+            async (payload) => {
+                console.log("New message received:", payload);
+                if (
+                    (
+                        payload.new as dbTypes.Database["public"]["Tables"]["messages"]["Row"]
+                    ).user_id === (await checkLoginUser())?.id
+                ) {
+                    return;
+                }
+                switch (payload.eventType) {
+                    case "INSERT":
+                        notifSound.play().catch((error) => {
+                            console.error(
+                                "Error playing notification sound:",
+                                error
+                            );
+                        });
+                        try {
+                            const message = row2Msg(
+                                payload.new as dbTypes.Database["public"]["Tables"]["messages"]["Row"]
+                            );
+                            addMessage(message);
+                        } catch (error) {
+                            console.error("Error parsing message:", error);
+                            showToast({
+                                title: "Error",
+                                message:
+                                    "Error parsing message from database. (" +
+                                    error.message +
+                                    ")",
+                                type: "error",
+                                duration: 5000,
+                            });
+                        }
+                        break;
+                    case "UPDATE":
+                        // find the message in the chat and update it
+                        console.log("Update event received:", payload);
+                        const updatedMessage = row2Msg(
                             payload.new as dbTypes.Database["public"]["Tables"]["messages"]["Row"]
                         );
-                        addMessage(message);
-                    } catch (error) {
-                        console.error("Error parsing message:", error);
+                        const messageElement = chat.querySelector(
+                            'div.message[data-message-id="' +
+                                updatedMessage.id +
+                                '"]'
+                        ) as HTMLDivElement | null;
+                        if (messageElement) {
+                            const contentElement =
+                                messageElement.querySelector("span");
+                            if (contentElement) {
+                                contentElement.textContent =
+                                    updatedMessage.content;
+                            }
+                            const userElement = messageElement.querySelector(
+                                ".user"
+                            ) as HTMLSpanElement | null;
+                            if (userElement) {
+                                userElement.textContent = await getUserName(
+                                    updatedMessage.user_id
+                                );
+                                userElement.style.color = usernameColor(
+                                    userElement.textContent!
+                                );
+                            }
+                            const timestampElement =
+                                messageElement.querySelector("time");
+                            if (timestampElement) {
+                                timestampElement.setAttribute(
+                                    "datetime",
+                                    updatedMessage.timestamp
+                                );
+                                timestampElement.innerHTML = humanize(
+                                    updatedMessage.timestamp
+                                );
+                            }
+                        } else {
+                            console.warn(
+                                "Message to update not found in chat:",
+                                updatedMessage.id
+                            );
+                        }
+
+                        break;
+                    case "DELETE":
+                        const deletedMessageId = (
+                            payload.old as dbTypes.Database["public"]["Tables"]["messages"]["Row"]
+                        ).id;
+                        const deletedMessageElement = chat.querySelector(
+                            'div.message[data-message-id="' +
+                                deletedMessageId +
+                                '"]'
+                        ) as HTMLDivElement | null;
+                        if (deletedMessageElement) {
+                            deletedMessageElement.remove();
+                        } else {
+                            console.warn(
+                                "Message to delete not found in chat:",
+                                deletedMessageId
+                            );
+                        }
+                        break;
+                    default:
+                        // This should not happen, but just in case
+                        console.error(
+                            "Unknown event type:",
+                            (payload as { eventType: string }).eventType
+                        );
                         showToast({
                             title: "Error",
                             message:
-                                "Error parsing message from database. (" +
-                                error.message +
-                                ")",
+                                "Unknown event type received from database.",
                             type: "error",
                             duration: 5000,
                         });
-                    }
-                    break;
-                case "UPDATE":
-                    // find the message in the chat and update it
-                    console.log("Update event received:", payload);
-                    const updatedMessage = row2Msg(
-                        payload.new as dbTypes.Database["public"]["Tables"]["messages"]["Row"]
-                    );
-                    const messageElement = chat.querySelector('div.message[data-message-id="' + updatedMessage.id + '"]') as HTMLDivElement | null;
-                    if (messageElement) {
-                        const contentElement = messageElement.querySelector("span");
-                        if (contentElement) {
-                            contentElement.textContent = updatedMessage.content;
-                        }
-                        const userElement = messageElement.querySelector(".user") as HTMLSpanElement | null;
-                        if (userElement) {
-                            userElement.textContent = await getUserName(updatedMessage.user_id);
-                            userElement.style.color = usernameColor(userElement.textContent!);
-                        }
-                        const timestampElement = messageElement.querySelector("time");
-                        if (timestampElement) {
-                            timestampElement.setAttribute("datetime", updatedMessage.timestamp);
-                            timestampElement.innerHTML = humanize(updatedMessage.timestamp);
-                        }
-                    } else {
-                        console.warn("Message to update not found in chat:", updatedMessage.id);
-                    }
-
-                    break;
-                case "DELETE":
-                    const deletedMessageId = (payload.old as dbTypes.Database["public"]["Tables"]["messages"]["Row"]).id;
-                    const deletedMessageElement = chat.querySelector('div.message[data-message-id="' + deletedMessageId + '"]') as HTMLDivElement | null;
-                    if (deletedMessageElement) {
-                        deletedMessageElement.remove();
-                    } else {
-                        console.warn("Message to delete not found in chat:", deletedMessageId);
-                    }
-                    break;
-                default:
-                    // This should not happen, but just in case
-                    console.error("Unknown event type:", (payload as { eventType: string }).eventType);
-                    showToast({
-                        title: "Error",
-                        message: "Unknown event type received from database.",
-                        type: "error",
-                        duration: 5000,
-                    });
-                    break;
+                        break;
+                }
             }
+        )
+        .subscribe();
+
+    requireFinishedAsync(async () => {
+        const { data, error } = await supabase
+            .from("messages")
+            .select("*")
+            .order("timestamp", { ascending: false })
+            .limit(500);
+
+        if (error) {
+            console.error("Error fetching messages:", error);
         }
-    )
-    .subscribe();
-
-    const { data, error } = await supabase
-        .from('messages')
-        .select('*')
-        .order('timestamp', { ascending: false })
-        .limit(500);
-
-    if (error) {
-        console.error("Error fetching messages:", error);
-    }
-    if (data) {
-        requireFinishedAsync(async () => {
+        if (data) {
             data.reverse();
             for (const message of data) {
                 const parsedMessage = row2Msg(message);
                 await addMessage(parsedMessage);
             }
-        });
-    }
-}, 'main-chat-init');
+        }
+    });
+}, "main-chat-init");
 
-function row2Msg(row: dbTypes.Tables<'messages'>) {
+function row2Msg(row: dbTypes.Tables<"messages">) {
     row.timestamp = new Date(row.timestamp).toISOString();
     return row;
 }
@@ -247,7 +299,7 @@ async function addMessage(message) {
     const deleteButton = document.createElement("button");
     deleteButton.className = "delete-button";
     deleteButton.title = "Delete";
-    deleteButton.innerHTML = `<svg class="fa-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M170.5 51.6L151.5 80l145 0-19-28.4c-1.5-2.2-4-3.6-6.7-3.6l-93.7 0c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80 368 80l48 0 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-8 0 0 304c0 44.2-35.8 80-80 80l-224 0c-44.2 0-80-35.8-80-80l0-304-8 0c-13.3 0-24-10.7-24-24S10.7 80 24 80l8 0 48 0 13.8 0 36.7-55.1C140.9 9.4 158.4 0 177.1 0l93.7 0c18.7 0 36.2 9.4 46.6 24.9zM80 128l0 304c0 17.7 14.3 32 32 32l224 0c17.7 0 32-14.3 32-32l0-304L80 128zm80 64l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0l0 208c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-208c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>`;
+    deleteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon"><path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" /></svg>`;
     deleteButton.addEventListener("click", async () => {
         const user = await checkLoginUser();
         if (!user || user.id !== message.user_id) {
@@ -285,7 +337,7 @@ async function addMessage(message) {
     const editButton = document.createElement("button");
     editButton.className = "edit-button";
     editButton.title = "Edit";
-    editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="fa-icon"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>`;
+    editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="icon"><path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" /><path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" /></svg>`;
     editButton.addEventListener("click", async () => {
         const user = await checkLoginUser();
         if (!user || user.id !== message.user_id) {
@@ -304,7 +356,8 @@ async function addMessage(message) {
         if (newContent.length > 500) {
             showToast({
                 title: "Error",
-                message: "Message is too long. Maximum length is 500 characters.",
+                message:
+                    "Message is too long. Maximum length is 500 characters.",
                 type: "error",
                 duration: 5000,
             });
@@ -328,12 +381,17 @@ async function addMessage(message) {
         contentElement.textContent = newContent;
     });
     actions.appendChild(editButton);
-    messageElement.appendChild(actions);
+    if (
+        (await checkLogin()) &&
+        message.user_id === (await checkLoginUser())?.id
+    ) {
+        messageElement.appendChild(actions);
+    }
     const contentElement = document.createElement("span");
     contentElement.textContent = message.content;
     messageElement.appendChild(contentElement);
     chat.appendChild(messageElement);
-    chat.scrollTo({ top: chat.scrollHeight, behavior: 'auto' });
+    chat.scrollTo({ top: chat.scrollHeight, behavior: "auto" });
 }
 
 requireFinishedAsync(async () => {
@@ -347,6 +405,8 @@ requireFinishedAsync(async () => {
 });
 
 onAllFinished(() => {
-    const overlay = document.getElementById("loading-overlay") as HTMLDivElement;
+    const overlay = document.getElementById(
+        "loading-overlay"
+    ) as HTMLDivElement;
     overlay.hidden = true;
 });
